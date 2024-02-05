d44bb30b9d2d3d4a8f90d18059e02b2529dc471e
Younas <younaslakziz@gmail.com>
2024-02-05 21:23:08 +0100

    refactor(gathering): Add Gathering module remove unused app folder files

--- libs/gathering/infrastructure/src/index.ts
+++ libs/gathering/infrastructure/src/index.ts
@@ -1,2 +1,1 @@
-export { CreateSessionModule } from './primary-adapters/use-cases/create-session/create-session.module';
-export { GetMatchingSessionModule } from './primary-adapters/use-cases/get-matching-session/get-matching-session.module';
+export { GatheringModule } from './primary-adapters/gathering.module';
